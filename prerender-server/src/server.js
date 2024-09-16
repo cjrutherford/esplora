2da522185be76c360150611a91b1ef3b0e14a6af
chris rutherford <crutherford@blockstream.com>
2024-09-16 12:27:53 -0400

    091624-1227et updated dependencies, and started surveying the prerender server

--- prerender-server/src/server.js
+++ prerender-server/src/server.js
@@ -3,21 +3,23 @@ import pug from 'pug'
 import path from 'path'
 import express from 'express'
 import request from 'superagent'
+import apicache from 'apicache';
 
 import l10n from '../client/l10n'
 import render from '../client/run-server'
 
-const themes = [ 'light', 'dark' ]
-    , langs = Object.keys(l10n)
-    , baseHref = process.env.BASE_HREF || '/'
-    , canonBase = process.env.CANONICAL_URL ? process.env.CANONICAL_URL.replace(/\/$/, '') : null
-    , apiUrl = process.env.API_URL.replace(/\/$/, '')
+const themes = ['light', 'dark']
+  , langs = Object.keys(l10n)
+  , baseHref = process.env.BASE_HREF || '/'
+  , canonBase = process.env.CANONICAL_URL ? process.env.CANONICAL_URL.replace(/\/$/, '') : null
+  , apiUrl = process.env.API_URL.replace(/\/$/, '')
 
 const rpath = p => path.join(__dirname, p)
 
 const indexView = rpath('../../client/index.pug')
 
 const app = express()
+const cache = apicache.middleware
 app.engine('pug', pug.__express)
 
 if (app.settings.env == 'development')
@@ -25,6 +27,8 @@ if (app.settings.env == 'development')
 
 app.use(require('cookie-parser')())
 app.use(require('body-parser').urlencoded({ extended: false }))
+
+app.use(cache('5 minutes'));
 
 app.use((req, res, next) => {
   // TODO: optimize /block-height/nnn (no need to render the whole app just to get the redirect)
@@ -37,23 +41,28 @@ app.use((req, res, next) => {
   if (!langs.includes(lang)) lang = 'en'
   if (req.query.lang && req.cookies.lang !== lang) res.cookie('lang', lang)
 
-  render(req._parsedUrl.pathname, req._parsedUrl.query || '', req.body, { theme, lang }, (err, resp) => {
-    if (err) return next(err)
-    if (resp.redirect) return res.redirect(301, baseHref + resp.redirect.substr(1))
-    if (resp.errorCode) {
-      console.error(`Failed with code ${resp.errorCode}:`, resp)
-      return res.sendStatus(resp.errorCode)
-    }
+  render(
+    req._parsedUrl.pathname, 
+    req._parsedUrl.query || '', 
+    req.body, 
+    { theme, lang }, 
+    (err, resp) => {
+      if (err) return next(err)
+      if (resp.redirect) return res.redirect(301, baseHref + resp.redirect.substr(1))
+      if (resp.errorCode) {
+        console.error(`Failed with code ${resp.errorCode}:`, resp)
+        return res.sendStatus(resp.errorCode)
+      }
 
-    res.status(resp.status || 200)
-    res.render(indexView, {
-      prerender_title: resp.title
-    , prerender_html: resp.html
-    , canon_url: canonBase ? canonBase + req.url : null
-    , noscript: true
-    , theme
-    , t: l10n[lang]
-    })
+      res.status(resp.status || 200)
+      res.render(indexView, {
+        prerender_title: resp.title
+        , prerender_html: resp.html
+        , canon_url: canonBase ? canonBase + req.url : null
+        , noscript: true
+        , theme
+        , t: l10n[lang]
+      })
   })
 
 })
@@ -64,10 +73,10 @@ if (process.env.SOCKET_PATH) {
     if (fs.statSync(process.env.SOCKET_PATH).isSocket()) {
       fs.unlinkSync(process.env.SOCKET_PATH)
     }
-  } catch (_) {}
+  } catch (_) { }
 }
 
-app.listen(process.env.SOCKET_PATH || process.env.PORT || 5001, function(){
+app.listen(process.env.SOCKET_PATH || process.env.PORT || 5001, function () {
   let addr = this.address()
   if (addr.address) addr = `${addr.address}:${addr.port}`
   console.log(`HTTP server running on ${addr}`)
